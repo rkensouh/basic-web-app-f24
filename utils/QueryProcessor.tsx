15440868880526a9eb651c09c57a5e92170bfa81
Reem Konsowa <reemkonsowa@dyn-172-20-101-34.qatar.cmu.local>
2024-10-24 16:32:45 +0300

    what is your name4

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,7 +12,7 @@ export default function QueryProcessor(query: string): string {
     //TODO update the corresponding test case in __tests__
     return ( "rkensouh" );
   }
-  if (query.toLowerCase().includes("What is your name?")) {
+  if (query.includes("What is your name?")) {
     // TODO: add your name or handle here
     // TODO: update the corresponding test case in __tests__
     return "rkensouh-313"; // Replace "YourName" with your actual name or handle
